f07a48a982c1cf66bc55da3f9f8ca0268670f667
Leto2007z <chekhlovni@gmail.com>
2024-08-16 17:55:27 +0300

    client.lua

--- client.lua
+++ client.lua
@@ -1,13 +1,27 @@
 RegisterCommand('giveweapon', function (player, args)
     local weaponName = args[1]
 
-    if not args then
+    if not weaponName then
         TriggerEvent('chat:addMessage', {
-            args = {'Не сущетсвует оружия '..weaponName..'.'}
+            args = {'Введите название оружия.'}
         })
         return
     end
 
     RegisterNetEvent('ch_giveweapon', weaponName)
     TriggerServerEvent('ch_giveweapon', weaponName)
+end)
+
+RegisterCommand ('giveweaponto', function(player,args)
+    local targetId = args[1]
+
+    if not targetId then
+        TriggerEvent('chat:addMessage', {
+            args{'Введите ID игрока.'}
+        })
+        return
+    end
+
+    RegisterNetEvent('ch_giveweaponto', targetId)
+    TriggerServerEvent('ch_giveweaponto', targetId)
 end)
